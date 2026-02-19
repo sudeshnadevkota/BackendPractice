05bcb4285e336b66690c0c58dd894710bea9a9c3
sudeshnadevkota <sudeshnadevkota@gmail.com>
2026-02-19 14:18:26 +0545

    register function

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,40 @@
 const User = require(".../model/userModel");
 
 const register = async(req, res) => {
+//Destructuring the data
+const{ firstName, lastName, email, password}=req.body;
+try{
+    if( !firstName || !lastName || !email || !password ){
+    return res.status(400).json({
+    success: false,
+    message: "All fields are required"
+});
+    };
+const existingEmail= await User.findOne({email: email});
+if(existingEmail){
+      return res.status(400).json({
+    success: false,
+    message: "Email already exist....."
+});
+};
 
+const newUser = new User({
+    firstName, lastName, email, password
+});
+
+await newUser.save();
+    return res.status(201).json({
+    success: true,
+    message: "User Registeres Successfully...",
+    newUser
+});
+
+   } catch(error){
+    return res.status(500).json({
+    success: false,
+    message: `Internal Server error ${error}`
+});
+}
 }
 const login = async(req, res) => {
 
---
+++ model/ProductModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true
+    },
+
+})
+module.exports = mongoose.model("Product", productSchema);
